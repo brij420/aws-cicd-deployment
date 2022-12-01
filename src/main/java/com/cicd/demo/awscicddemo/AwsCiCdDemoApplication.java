7d9f14706f1eb326e82f0365369fc0706081f1a5
Brijesh Kumar <brij420@gmail.com>
2022-12-01 13:27:55 +0100

    initial commit

---
+++ src/main/java/com/cicd/demo/awscicddemo/AwsCiCdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.demo.awscicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCiCdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCiCdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicd/demo/awscicddemo/AwsCiCdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.demo.awscicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCiCdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
