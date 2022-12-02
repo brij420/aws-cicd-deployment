73c9c6f8667204def31d5429b460425584a6148c
Brijesh Kumar <brij420@gmail.com>
2022-12-02 17:50:38 +0100

    Update fix_privileges.sh

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/server/* jar
-chmod +x /home/ec2-user/server/server_start.sh
-chmod +x /home/ec2-user/server/server_stop.sh
+sudo chmod +x /home/ec2-user/server/* jar
+sudo chmod +x /home/ec2-user/server/server_start.sh
+sudo chmod +x /home/ec2-user/server/server_stop.sh
