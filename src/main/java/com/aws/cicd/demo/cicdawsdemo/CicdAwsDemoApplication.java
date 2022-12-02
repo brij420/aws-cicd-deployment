875c3f13a680d10506e603dc4c582c79f7430c2a
Brijesh Kumar <brij420@gmail.com>
2022-12-02 09:42:04 +0100

    new

---
+++ src/main/java/com/aws/cicd/demo/cicdawsdemo/CicdAwsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aws.cicd.demo.cicdawsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdAwsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdAwsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/cicd/demo/cicdawsdemo/CicdAwsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.cicd.demo.cicdawsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdAwsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
